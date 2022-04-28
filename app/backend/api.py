43aea4bad5d0078f81f724ea5f9de6a3d95f0af7
Thavarasa Prasanth <45243326+pthavarasa@users.noreply.github.com>
2022-04-28 19:40:33 +0200

    api imprmented with fastapi

---
+++ app/backend/api.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import pickle
+import pandas as pd
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=origins,
+    allow_headers=origins,
+)
+
+
+class Item(BaseModel):
+    boulangerie: float
+    ecole: float
+    monument: float
+    superette: float
+    terrain: float
+    wc: float
+    bathtub: float
+    bedrooms: float
+    volume: float
+    rooms: float
+    district16: float
+
+
+filename = '../../model/xgboost_regression_model_min.pickle'
+classifier = pickle.load(open(filename, "rb"))
+
+
+@app.post("/predict/")
+async def create_item(item: Item):
+    return {"price": classifier.predict([list(item.__dict__.values())]).tolist()[0]}
